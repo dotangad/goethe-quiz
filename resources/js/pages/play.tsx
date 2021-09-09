84a6afd375635577b319338d04dbb879aab7b6c3
Angad Singh <mail@angad.dev>
2021-09-10 03:02:39 +0530

    ui: start play page

--- resources/js/pages/play.tsx
+++ resources/js/pages/play.tsx
@@ -1,12 +1,47 @@
+import { usePage } from "@inertiajs/inertia-react";
+import { Inertia, Method } from "@inertiajs/inertia";
+import { compareAsc } from "date-fns";
 import React from "react";
 import Layout from "../components/Layout";
+import { IPageProps } from "../lib/types";
 
 const Play: React.FC = () => {
+  const {
+    props: { user, endDate, startDate },
+  } = usePage<IPageProps>();
+  const [started, setStarted] = React.useState<boolean>(
+    compareAsc(new Date(), new Date(startDate)) === 1
+  );
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setStarted(compareAsc(new Date(), new Date(startDate)) == 1);
+      if (compareAsc(new Date(), new Date(endDate)) == 1) {
+        clearInterval(interval);
+        Inertia.visit("/auth/logout", { method: "get" as Method });
+      }
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <Layout links={[{ href: "/leaderboard", label: "Leaderboard" }]}>
       <div className="flex w-full h-full items-center justify-center">
-        <div className="bg-white border-none border-gray-200 rounded-lg w-full max-w-sm p-6 mx-2 shadow-sm">
-          hello
+        <div className="bg-white border-none border-gray-200 rounded-lg w-full max-w-md p-6 mx-2 shadow-sm">
+          {!started ? (
+            <div className="font-bold text-center text-md">
+              DPS Goethe Quiz has not started yet
+            </div>
+          ) : (
+            <div>play component</div>
+          )}
+
+          <div className="mt-4">
+            <div className="text-xs text-center">
+              {user.school?.name} - {user.student_1} and {user.student_2}
+            </div>
+          </div>
         </div>
       </div>
     </Layout>
