037099eb3d9d48c077a97fbe304a558e112cea53
Angad Singh <mail@angad.dev>
2021-10-18 20:24:09 +0530

    fix: favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,12 +8,11 @@
   <script src="{{ mix('/js/app.js') }}" defer></script>
   <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
   <title>DPS Goethe Quiz</title>
-  {/*
-  <link rel="icon" type="image/png" href="/img/goethelogo-square.png" />*/}
-  <link rel="apple-touch-icon" sizes="180x180" href="https://goethe.de/resources/relaunch/favicon/apple-touch-icon.png" />
-  <link rel="icon" type="image/png" sizes="32x32" href="https://goethe.de/resources/relaunch/favicon/favicon-32x32.png" />
-  <link rel="icon" type="image/png" sizes="16x16" href="https://goethe.de/resources/relaunch/favicon/favicon-16x16.png" />
-  <link rel="shortcut icon" href="https://goethe.de/resources/relaunch/favicon/favicon.ico" />
+  <!-- <link rel="icon" type="image/png" href="/img/goethelogo-square.png"> -->
+  <link rel="apple-touch-icon" sizes="180x180" href="https://goethe.de/resources/relaunch/favicon/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="https://goethe.de/resources/relaunch/favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="https://goethe.de/resources/relaunch/favicon/favicon-16x16.png">
+  <link rel="shortcut icon" href="https://goethe.de/resources/relaunch/favicon/favicon.ico">
 </head>
 
 <body class="bg-gray-bg">
