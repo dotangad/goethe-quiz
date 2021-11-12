9ab267946df851a08661d56b3afc45d08b229961
Kavin Valli <kavinvalli@gmail.com>
2021-11-12 20:25:53 +0530

    fix: same class name issue

--- app/Console/Commands/GeneratePasswordUser.php
+++ app/Console/Commands/GeneratePasswordUser.php
@@ -6,7 +6,7 @@ use App\Models\User;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Hash;
 
-class GeneratePasswordEmail extends Command
+class GeneratePasswordUser extends Command
 {
   /**
    * The name and signature of the console command.
