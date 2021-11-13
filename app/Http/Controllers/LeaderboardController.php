ea697f554024642a917832da362b3efd8ed64d1c
Angad Singh <mail@angad.dev>
2021-11-13 13:10:40 +0530

    feat: add finished users to the leaderboard

--- app/Http/Controllers/LeaderboardController.php
+++ app/Http/Controllers/LeaderboardController.php
@@ -12,21 +12,38 @@ class LeaderboardController extends Controller
   public function index()
   {
     return Inertia::render('leaderboard', [
-      'users' => User::with('school')
-        ->where('type', 'team')
-        ->orderBy('question_id', 'DESC')
-        ->orderBy('last_solved', 'ASC')
-        // ->get(['id', 'question_id', 'student_name'])
-        ->take(100)
-        ->get()
-        ->map(function ($record) {
-          return [
-            'id' => $record->id,
-            'question_id' => $record->question_id,
-            'student_name' => $record->student_name,
-            'school' => $record->school->name
-          ];
-        })
+      'users' => array_merge(
+        User::with('school')
+          ->where('type', 'team')
+          ->whereNull('question_id')
+          ->orderBy('last_solved', 'ASC')
+          ->get()
+          ->map(function ($record) {
+            return [
+              'id' => $record->id,
+              'question_id' => '-',
+              'student_name' => $record->student_name,
+              'school' => $record->school->name
+            ];
+          })
+          ->toArray(),
+        User::with('school')
+          ->where('type', 'team')
+          ->orderBy('question_id', 'DESC')
+          ->orderBy('last_solved', 'ASC')
+          // ->get(['id', 'question_id', 'student_name'])
+          ->take(100)
+          ->get()
+          ->map(function ($record) {
+            return [
+              'id' => $record->id,
+              'question_id' => $record->question_id,
+              'student_name' => $record->student_name,
+              'school' => $record->school->name
+            ];
+          })
+          ->toArray()
+      )
     ]);
   }
   public function leaderboard2()
